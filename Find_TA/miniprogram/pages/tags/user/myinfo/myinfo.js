a1015dec0a51d62718d0d4e36e1c9cf40c639d91
Andrew-xj <544876317@qq.com>
2019-02-16 10:08:18 +0800

    myinfo page finished

--- Find_TA/miniprogram/pages/tags/user/myinfo/myinfo.js
+++ Find_TA/miniprogram/pages/tags/user/myinfo/myinfo.js
@@ -1,106 +1,99 @@
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    showTopTips: false,
-
-    radioItems: [
-      { name: 'cell standard', value: '0' },
-      { name: 'cell standard', value: '1', checked: true }
-    ],
-    checkboxItems: [
-      { name: 'standard is dealt for u.', value: '0', checked: true },
-      { name: 'standard is dealicient for u.', value: '1' }
-    ],
-
-    date: "2016-09-01",
-    time: "12:01",
-
-    countryCodes: ["+86", "+80", "+84", "+87"],
-    countryCodeIndex: 0,
-
-    countries: ["中国", "美国", "英国"],
-    countryIndex: 0,
-
-    accounts: ["微信号", "QQ", "Email"],
-    accountIndex: 0,
-
-    isAgree: false
+    name:'',
+    school:'',
+    card:'',
+    qq:'',
+    phone:''
   },
-  showTopTips: function () {
-    var that = this;
-    this.setData({
-      showTopTips: true
-    });
-    setTimeout(function () {
-      that.setData({
-        showTopTips: false
-      });
-    }, 3000);
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
   },
-  radioChange: function (e) {
-    console.log('radio发生change事件，携带value值为：', e.detail.value);
 
-    var radioItems = this.data.radioItems;
-    for (var i = 0, len = radioItems.length; i < len; ++i) {
-      radioItems[i].checked = radioItems[i].value == e.detail.value;
-    }
-
-    this.setData({
-      radioItems: radioItems
-    });
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
   },
-  checkboxChange: function (e) {
-    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
 
-    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
-    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
-      checkboxItems[i].checked = false;
-
-      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
-        if (checkboxItems[i].value == values[j]) {
-          checkboxItems[i].checked = true;
-          break;
-        }
-      }
-    }
-
-    this.setData({
-      checkboxItems: checkboxItems
-    });
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
   },
-  bindDateChange: function (e) {
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  name: function (e) {
     this.setData({
-      date: e.detail.value
+      name: e.detail.value
     })
   },
-  bindTimeChange: function (e) {
-    this.setData({
-      time: e.detail.value
-    })
-  },
-  bindCountryCodeChange: function (e) {
-    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
 
+  school: function (e) {
     this.setData({
-      countryCodeIndex: e.detail.value
+      school: e.detail.value
     })
   },
-  bindCountryChange: function (e) {
-    console.log('picker country 发生选择改变，携带值为', e.detail.value);
 
+  card: function (e) {
     this.setData({
-      countryIndex: e.detail.value
+      card: e.detail.value
     })
   },
-  bindAccountChange: function (e) {
-    console.log('picker account 发生选择改变，携带值为', e.detail.value);
 
+  qq: function (e) {
     this.setData({
-      accountIndex: e.detail.value
+      qq: e.detail.value
     })
   },
-  bindAgreeChange: function (e) {
+
+  phone: function (e) {
     this.setData({
-      isAgree: !!e.detail.value.length
-    });
-  }
-});
+      phone: e.detail.value
+    })
+  },
+})
